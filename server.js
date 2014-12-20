bfc07557c14710dcad483d7a93e654eadcb0a877
Matheus Guilarducci <mguilarducci@gmail.com>
2014-12-20 11:49:59 -0200

    tirando modo debug

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ var init = require('./config/init')(),
  * Please note that the order of loading is important.
  */
 
-mongoose.set('debug', true);
-
 // Bootstrap db connection
 var db = mongoose.connect(config.db, function(err) {
 	if (err) {
